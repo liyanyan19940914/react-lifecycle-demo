938f4308dd25a490e61ceb5bf58f4562ddd3ae39
liyanyan19940914 <921258351@qq.com>
2017-05-24 18:00:20 +0800

    lifecycle

---
+++ README.md
@@ -0,0 +1,1 @@
+# lab-fake-golden-data-with-react
---
+++ js/lifecycle.js
@@ -0,0 +1,62 @@
+var Counter = React.createClass({
+    getInitialState() {
+        console.log('getInitialState');
+        return {value: 0};
+    },
+    plus() {
+        this.setState({
+            value: ++this.state.value
+        });
+    },
+    minus() {
+        this.setState({
+            value: --this.state.value
+        });
+    },
+    componentWillMount() {
+        console.log('Component WILL MOUNT!')
+    },
+
+    componentDidMount() {
+        console.log('Component DID MOUNT!')
+    },
+
+    componentWillReceiveProps() {
+        console.log('Component WILL RECIEVE PROPS!')
+    },
+
+    shouldComponentUpdate() {
+        console.log('should Component Update!');
+        return true;
+        // return false;
+    },
+
+    componentWillUpdate() {
+        console.log('Component WILL UPDATE!');
+    },
+
+    componentDidUpdate() {
+        console.log('Component DID UPDATE!')
+    },
+
+    componentWillUnmount() {
+        console.log('Component WILL UNMOUNT!')
+    },
+
+    render: function() {
+        return (
+            <div>
+                <button onClick={this.plus}>+</button>
+                <span>{this.state.value}</span>
+                {console.log('render')}
+                <button onClick={this.minus}>-</button>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <div>
+        <Counter/>
+    </div>,document.getElementById('content')
+);
